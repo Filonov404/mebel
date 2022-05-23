db64b8d50ec5cf4d34860ea36fd76777c588c404
filonov404 <fffilonov31@gmail.com>
2022-05-23 11:36:00 +0300

    refactor mobile

--- js/main.js
+++ js/main.js
@@ -1,61 +1,138 @@
-$(function(){
-    $('.menu__btn').on('click', function(){
-        $('.menu__list').toggleclass('active');
-    });
+$(function () {
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleclass("active");
+  });
 
-    $(".star").rateYo({
-        ratedFill: "#efc162",
-        "starSvg": "<?xml version=\"1.0\" ?><svg id=\"Layer_1\" style=\"enable-background:new 0 0 612 792;\" version=\"1.1\" viewBox=\"0 0 612 792\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"><style type=\"text/css\">\n" +
-            "\t.st0{fill:#FCAF17;}\n" +
-            "</style><g><polygon class=\"st0\" points=\"306,153 216.4,298.9 50,339 161,469.3 147.8,639.9 306,574.6 464.2,639.9 451,469.3 562,339    395.6,298.9 306,153  \"/></g></svg>"
-    });
+  $(".user-nav").on("click", function () {
+    $(".menu-wrapper").toggleClass("active");
+  });
 
-    $(".star-product").rateYo({
-        ratedFill: "#efc162",
-        "starSvg": "<?xml version=\"1.0\" ?><svg id=\"Layer_1\" style=\"enable-background:new 0 0 612 792;\" version=\"1.1\" viewBox=\"0 0 612 792\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"><style type=\"text/css\">\n" +
-            "\t.st0{fill:#FCAF17;}\n" +
-            "</style><g><polygon class=\"st0\" points=\"306,153 216.4,298.9 50,339 161,469.3 147.8,639.9 306,574.6 464.2,639.9 451,469.3 562,339    395.6,298.9 306,153  \"/></g></svg>"
-    });
+  $(".filter-trigger").on("click", function () {
+    $(".filter-wrapper").css("display", "block");
+  });
 
-    $('.slider-thumbs').slick({
-        vertical: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        focusOnSelect: true,
-        asNavFor: '.slider-main-items'
-    });
-    $('.slider-main-items').slick({
-        vertical: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        asNavFor: '.slider-thumbs'
-    });
+  $(".close-filtr").on("click", function () {
+    $(".filter-wrapper").css("display", "none");
+  });
 
-    $('.related-products-slider').slick({
-        slidesToShow: 4,
-        infinite: true,
-        autoplay: true
-    });
+  $(".close-blog-filtr").on("click", function () {
+    $(".filter-blog-inner").css("display", "none");
+  });
 
-    $('.slider-tabs').each(function() {
-        let ths = $(this);
-        ths.find('.slider-text-description').not(':first').hide();
-        ths.find('.tabs').click(function() {
-            ths.find('.tabs').removeClass('active').eq($(this).index()).addClass('active');
-            ths.find('.slider-text-description').hide().eq($(this).index()).fadeIn()
-        }).eq(0).addClass('active');
-    });
+  $(".filter-trigger").on("click", function () {
+    $(".filter-blog-inner").css("display", "block");
+  });
 
+  $(".footer-title").on("click", function () {
+    $(this).next().slideToggle();
+    $(this).toggleClass("active");
+  });
 
-    $(".js-range-slider").ionRangeSlider({
-        type: "double",
-        min: 1000,
-        max: 40000,
-        from: 10000,
-        to: 30000,
-        drag_interval: true,
-        min_interval: null,
-        max_interval: null
-    });
+  $(".container-slider").slick({
+    dots: true,
+    arrows: false,
+  });
+
+  $(".slider-main-wrapper").slick({
+    slidesToShow: 1,
+    infinite: true,
+    centerMode: true,
+    arrows: false,
+  });
+
+  $(".random-slider-wrapper").slick({
+    slidesToShow: 2,
+   
+    responsive: [
+      
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        },
+      },
+    ],
+  });
+
+  $(".star").rateYo({
+    ratedFill: "#efc162",
+    starSvg:
+      '<?xml version="1.0" ?><svg id="Layer_1" style="enable-background:new 0 0 612 792;" version="1.1" viewBox="0 0 612 792" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">\n' +
+      "\t.st0{fill:#FCAF17;}\n" +
+      '</style><g><polygon class="st0" points="306,153 216.4,298.9 50,339 161,469.3 147.8,639.9 306,574.6 464.2,639.9 451,469.3 562,339    395.6,298.9 306,153  "/></g></svg>',
+  });
+
+  $(".star-product").rateYo({
+    ratedFill: "#efc162",
+    starSvg:
+      '<?xml version="1.0" ?><svg id="Layer_1" style="enable-background:new 0 0 612 792;" version="1.1" viewBox="0 0 612 792" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">\n' +
+      "\t.st0{fill:#FCAF17;}\n" +
+      '</style><g><polygon class="st0" points="306,153 216.4,298.9 50,339 161,469.3 147.8,639.9 306,574.6 464.2,639.9 451,469.3 562,339    395.6,298.9 306,153  "/></g></svg>',
+  });
+
+  $(".related-products-slider").slick({
+    slidesToShow: 4,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
+
+  $(".slider-tabs").each(function () {
+    let ths = $(this);
+    ths.find(".slider-text-description").not(":first").hide();
+    ths
+      .find(".tabs")
+      .click(function () {
+        ths
+          .find(".tabs")
+          .removeClass("active")
+          .eq($(this).index())
+          .addClass("active");
+        ths
+          .find(".slider-text-description")
+          .hide()
+          .eq($(this).index())
+          .fadeIn();
+      })
+      .eq(0)
+      .addClass("active");
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 1000,
+    max: 40000,
+    from: 10000,
+    to: 30000,
+    drag_interval: true,
+    min_interval: null,
+    max_interval: null,
+  });
 });
